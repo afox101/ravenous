e8707e10312124f43a5621528a0d25553be3e309
afox101 <afox@afox101.com>
2018-09-30 18:13:21 -0400

    Second Commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,30 +5,29 @@ import BusinessList from '../BusinessList/BusinessList';
 import Yelp from '../../util/Yelp'
 
 class App extends React.Component {
-  
   constructor(props) {
     super(props);
-    
+
     this.state = {
       businesses: []
-    }
-    this.searchYelp = this.searchYelp.bind(this)
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
   }
-  
+
   searchYelp(term, location, sortBy) {
     Yelp.search(term, location, sortBy).then(businesses => {
-      this.setState({
-        businesses: businesses
-      })
-    })
+      this.setState({businesses: businesses});
+    });
   }
+
   render() {
     return (
       <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar searchYelp={this.searchYelp}/>
-      <BusinessList businesses={this.state.businesses}/>
-    </div>  
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
 }
